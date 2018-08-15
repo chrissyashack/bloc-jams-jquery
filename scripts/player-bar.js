86074aa68d8862c49fe3d3661a4d117f662e4fcd
Christine Ashack <chrissy.ashack@gmail.com>
2018-08-14 20:04:51 -0500

    Assignment 10 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -9,9 +9,20 @@
       const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
       const nextSongIndex = currentSongIndex + 1;
       if(nextSongIndex >= album.songs.length) { return; }
-      
+
       const nextSong = album.songs[nextSongIndex];
       player.playPause(nextSong);
+    });
+
+    $('button#previous').on('click', function(){
+      if (player.playState !== 'playing') { return; }
+
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const previousSongIndex = currentSongIndex - 1;
+      if(currentSongIndex > 1) { return; }
+
+      const newSong = album.songs[previousSongIndex];
+      player.playPause(newSong);
     });
   });
 }
